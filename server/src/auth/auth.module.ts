a59e547a43def6b8be4aebd0212de52efe94bfe4
전병길 <jeonbg@kakao.com>
2021-11-25 22:13:59 +0900

    feature (#12) : 로그인 jwtService 추가

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UesrRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UesrRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'JBFRONT',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UesrRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
